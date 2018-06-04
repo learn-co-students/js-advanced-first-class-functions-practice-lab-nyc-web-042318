8a861b1b29414688e0b2077d1737ad63eed001de
Derek Chen <derekgc@icloud.com>
2018-06-04 14:28:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Code your solution in this file!
+function logDriverNames(drivers) {
+  const logName = function (el, i, arr) {
+    console.log(el.name);
+  }
+  drivers.forEach(logName);
+  return ;
+}
+
+
+function logDriversByHometown(drivers, town) {
+  const logTown = function (el, i, arr) {
+    if(el.hometown === town ){
+      console.log(el.name);
+    }
+  }
+  drivers.forEach(logTown);
+}
+
+function driversByRevenue(drivers) {
+  let temp = drivers.slice(0).sort(function (a, b) {
+      return a.revenue - b.revenue;
+  });
+  return temp;
+}
+
+function driversByName(drivers) {
+  let temp = drivers.slice(0).sort(function (a, b) {
+      return a.name.localeCompare(b.name);
+  });
+  return temp;
+}
+
+function totalRevenue(drivers) {
+  const reducer = function (agg, el, i , arr) {
+    return agg + el.revenue;
+  }
+  let result =  drivers.reduce(reducer,0);
+
+  return result;
+}
+
+function averageRevenue(drivers) {
+  const reducer = function (agg, el, i, arr) {
+    return agg*i/(i+1)+el.revenue/(i+1);
+  }
+  let result = drivers.reduce(reducer,0);
+  return result;
+}
